b1839f5a7ae4573d22f08bddd0f682c2af9c2b57
Pietro <pietro.abate@tezcore.com>
2019-11-13 19:10:45 +0100

    add wrapper script ocaml 32/64

--- packages/ocaml-android64.4.07.0/files/install.sh
+++ packages/ocaml-android64.4.07.0/files/install.sh
@@ -1,4 +1,6 @@
-#!/bin/sh -e
+#!/bin/bash -e
+
+set -x
 
 PREFIX="$1"
 
@@ -6,7 +8,18 @@ for pkg in bigarray bytes compiler-libs dynlink findlib stdlib str threads unix;
   cp -r "${PREFIX}/lib/${pkg}" "${PREFIX}/android-sysroot/lib/"
 done
 
+for bin in ocamlc ocamlopt ocamlcp ocamlmklib ocamlmktop ocamldoc ocamldep; do
+  rm -rf ${PREFIX}/android-sysroot/bin/${bin}
+
+cat << EOF > "${PREFIX}/android-sysroot/bin/${bin}"
+#!/bin/sh
+${PREFIX}/bin/ocamlrun "${PREFIX}/android-sysroot/bin/${bin}.byte" "\$@"
+EOF
+
+  chmod +x "${PREFIX}/android-sysroot/bin/${bin}"
+done
+
 mkdir -p "${PREFIX}/lib/findlib.conf.d"
 cp android.conf "${PREFIX}/lib/findlib.conf.d"
 
-(cd "${PREFIX}/android-sysroot/bin/" && ln -sf ocamlc.byte ocamlc)
+#(cd "${PREFIX}/android-sysroot/bin/" && ln -sf ocamlc.byte ocamlc)
